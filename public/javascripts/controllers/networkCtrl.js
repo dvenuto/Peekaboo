a8cd01f5f24a3f10abb2764ed833e9902d07a537
dvenuto <donovan.venuto@servicemesh.com>
2016-08-30 10:15:39 -0500

    Update networkCtrl.js

--- public/javascripts/controllers/networkCtrl.js
+++ public/javascripts/controllers/networkCtrl.js
@@ -1,6 +1,7 @@
 /**
  * Created by Gkumar on 4/19/16.
  */
+
 (function () {
     'use strict';
 
@@ -9,13 +10,15 @@
         .controller('networkCtrl', networkCtrl);
 
     networkCtrl.$inject = ['$log', '$scope', 'apiServices'];
-
+    
     function networkCtrl($log, $scope, apiServices) {
 
         //get details for network page
         apiServices.getNetworks()
             .then(function (data) {
-                $log.debug("display all received data from network", data);
+                $log.debug("display all received data from network", data);           
+                $scope.networks = data.assets;
+            
             });
     }
 })();
